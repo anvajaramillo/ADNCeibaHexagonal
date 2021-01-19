b8c4168dce332714a08170ab29872cce0a633384
Angie Vanessa Jaramillo Castrillon <angie.jaramillo@ceiba.com.co>
2021-01-18 21:37:25 -0500

    pruebas unitarias y de integracion

--- microservicio/infraestructura/src/test/java/com/ceiba/apuesta/controlador/ConsultaControladorApuestaTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/apuesta/controlador/ConsultaControladorApuestaTest.java
@@ -6,10 +6,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
